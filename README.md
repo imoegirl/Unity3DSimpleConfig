0f1ec7946dc9d8e487c5c7b6aa5c5ad10ea6f7fa
Solleter <Solleter@outlook.com>
2019-10-08 15:12:58 +0800

    Update

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # Unity3DSimpleConfig
-C# simple config on unity3d, game development
+> 在游戏开发过程中，有一些简单的配置，不需要走表，使用一个文本文件去配置，更方便。
+
+**1. 配置文件格式如下**
+
+<img src="https://i.loli.net/2019/10/08/SKyuweNVY7Q2gnv.png" align=left />
+
+**"[ ]"** 中的部分，为模块名称，像 **GameFrame** 这样没有在某个模块下的配置，为全局配置
+
+
+
+**2. 如何使用**
+
+> `注意：在游戏开始时，首先要读取配置文件，路径自己定义`
+
+```csharp
+string path = @"D:/Config.txt";
+SimpleConfig.Load(path);
+
+int gameFrame = SimpleConfig.GetIntValue("GameFrame");
+int loginChannel = SimpleConfig.GetIntValue("Login", "LoginChannel");
+string testUserName = SimpleConfig.GetStringValue("Login", "TestUserName");
+string testPassword = SimpleConfig.GetStringValue("Login", "TestPassword");
+float timeout = SimpleConfig.GetFloatValue("Net", "Timeout");
+bool dm = SimpleConfig.GetBoolValue("Debug", "DebugMode");
+bool dm2 = SimpleConfig.GetBoolValue("Debug", "DebugMode2");
+
+Debug.LogFormat("GameFrame: {0}", gameFrame);
+Debug.LogFormat("LoginChannel: {0}", loginChannel);
+Debug.LogFormat("testUserName: {0}", testUserName);
+Debug.LogFormat("testPassword: {0}", testPassword);
+Debug.LogFormat("timeout: {0}", timeout);
+Debug.LogFormat("dm: {0},  dm2: {1}", dm, dm2);
+```
+
---
+++ SimpleConfig.cs
@@ -0,0 +1,150 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public static class SimpleConfig
+{
+    private static Dictionary<string, Dictionary<string, string>> dataDict = new Dictionary<string, Dictionary<string, string>>();
+
+    public static void Load(string configFilePath)
+    {
+        var enumer = dataDict.GetEnumerator();
+        while (enumer.MoveNext())
+        {
+            enumer.Current.Value.Clear();
+        }
+        dataDict.Clear();
+
+        System.IO.StreamReader sr = new System.IO.StreamReader(configFilePath);
+        string line;
+        string currSection = null;
+        char[] splitor = new char[] { '=' };
+        while((line = sr.ReadLine()) != null)
+        {
+            line = line.Trim();
+            if (string.IsNullOrEmpty(line))
+            {
+                continue;
+            }
+
+            if(line.StartsWith("[") && line.EndsWith("]"))
+            {
+                currSection = line.Substring(1, line.Length - 2);
+                if (!dataDict.ContainsKey(currSection))
+                {
+                    dataDict.Add(currSection, new Dictionary<string, string>());
+                }
+            }
+            else
+            {
+                string[] keyAndValue = line.Split(splitor, 2);
+                if(keyAndValue == null || keyAndValue.Length < 2)
+                {
+                    continue;
+                }
+
+                if(currSection == null)
+                {
+                    currSection = "Global";
+                    if (!dataDict.ContainsKey(currSection))
+                    {
+                        dataDict.Add(currSection, new Dictionary<string, string>());
+                    }
+                }
+
+                string key = keyAndValue[0].Trim();
+                string value = keyAndValue[1].Trim();
+
+                if (dataDict[currSection].ContainsKey(key))
+                {
+                    Debug.LogErrorFormat("在 {0} 部分已经包含名为 {1} 的变量！", currSection, key);
+                    continue;
+                }
+
+                dataDict[currSection].Add(key, value);
+            }
+        }
+    }
+
+    public static int GetIntValue(string variable)
+    {
+        return GetIntValue("Global", variable);
+    }
+
+    public static int GetIntValue(string section, string variable)
+    {
+        if (dataDict.ContainsKey(section) && dataDict[section].ContainsKey(variable))
+        {
+            string rawValue = dataDict[section][variable];
+            int value = 0;
+            if (int.TryParse(rawValue, out value))
+            {
+                return value;
+            }
+        }
+
+        Debug.LogErrorFormat("解析 {0} 部分的 {1} int 值出错", section, variable);
+        return -1;
+    }
+
+
+    public static float GetFloatValue(string variable)
+    {
+        return GetFloatValue("Global", variable);
+    }
+
+    public static float GetFloatValue(string section, string variable)
+    {
+        if (dataDict.ContainsKey(section) && dataDict[section].ContainsKey(variable))
+        {
+            string rawValue = dataDict[section][variable];
+            float value = 0;
+            if (float.TryParse(rawValue, out value))
+            {
+                return value;
+            }
+        }
+
+        Debug.LogErrorFormat("解析 {0} 部分的 {1} float 值出错", section, variable);
+        return -1;
+    }
+
+
+    public static string GetStringValue(string variable)
+    {
+        return GetStringValue("Global", variable);
+    }
+
+    public static string GetStringValue(string section, string variable)
+    {
+        if (dataDict.ContainsKey(section) && dataDict[section].ContainsKey(variable))
+        {
+            string rawValue = dataDict[section][variable];
+            return rawValue;
+        }
+
+        Debug.LogErrorFormat("解析 {0} 部分的 {1} string 值出错", section, variable);
+        return string.Empty;
+    }
+
+    public static bool GetBoolValue(string variable)
+    {
+        return GetBoolValue("Global", variable);
+    }
+
+    public static bool GetBoolValue(string section, string variable)
+    {
+        if (dataDict.ContainsKey(section) && dataDict[section].ContainsKey(variable))
+        {
+            string rawValue = dataDict[section][variable].ToLower();
+            bool value = false;
+            if (bool.TryParse(rawValue, out value))
+            {
+                return value;
+            }
+        }
+
+        Debug.LogErrorFormat("解析 {0} 部分的 {1} bool 值出错", section, variable);
+        return false;
+    }
+}
